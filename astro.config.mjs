3024cc54c63aad4d196185e2c869811b78eef7f5
Boxersteavee <boxersgaming@hotmail.com>
2023-03-21 15:55:44 +0000

    Add: Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
-
+import partytown from "@astrojs/partytown";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+      partytown({
+        // Adds dataLayer.push as a forwarding-event.
+        config: {
+          forward: ["dataLayer.push"],
+        },
+      }),
+    ],
+  });
