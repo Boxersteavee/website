4ddebd993e846edea46b0727f52ae220fbcbb64e
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2023-09-10 17:30:10 +0100

    Fix: A few things.

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,4 +11,15 @@ export const collections = {
 			img_alt: z.string().optional(),
 		}),
 	}),
+
+	photography: defineCollection({
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
+	}),
 };
---
+++ src/content/photography/test.md
@@ -0,0 +1,14 @@
+---
+title: name
+publishDate: 2019-12-01 00:00:00
+img: "https://ik.imagekit.io/Boxersteavee/assets/Image-Goes-Here.jpg"
+img_alt: Alt text for the above image
+description: |
+  Description of page
+tags:
+  - Tags go
+  - Like This
+
+---
+
+test page
