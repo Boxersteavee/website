dd7baded4e37e8690a8328af5661dc3780cd50e2
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2023-10-12 20:54:54 +0100

    Revert adding tina

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,53 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: null,
+  // Get this from tina.io
+  token: null,
+  // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/content/projects",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "datetime",
+            name: "posted",
+            label: "Date Posted",
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
