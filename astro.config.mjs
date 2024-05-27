1bb31a8dd919005f19a48a3347405409e8229559
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2024-05-27 13:35:03 +0100

    Add: Cookie Prompt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
+import solidJs from "@astrojs/solid-js";
+
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-      partytown({
-        // Adds dataLayer.push as a forwarding-event.
-        config: {
-          forward: ["dataLayer.push"],
-        },
-      }),
-    ],
-  });
+  integrations: [partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), solidJs(), tailwind()]
+});
