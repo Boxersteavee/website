e8350f997b38aad93ae3d8ba0ce2e9a9a88a2c2f
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2024-05-28 13:31:03 +0100

    Revert "Added Cloudflare for Decap OAuth?"
    This reverts commit 297398c15f39dacd74218bf52b4e5ca6204af4ec.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,27 +4,22 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
-import cloudflare from "@astrojs/cloudflare";
+
 const file = fileURLToPath(new URL('package.json', import.meta.url));
 const json = readFileSync(file, 'utf8');
 const pkg = JSON.parse(json);
 
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
   vite: {
     define: {
-      "import.meta.env.PKG_VERSION": JSON.stringify(pkg.version)
-    }
+      "import.meta.env.PKG_VERSION": JSON.stringify(pkg.version),
+    },
   },
   integrations: [partytown({
     // Adds dataLayer.push as a forwarding-event.
     config: {
       forward: ["dataLayer.push"]
     }
-  }), solidJs(), tailwind()],
-  output: "server",
-  adapter: cloudflare()
+  }), solidJs(), tailwind()]
 });
