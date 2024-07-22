2522ae46cbbd1b0c400e97f27df3650fd2e86212
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2024-07-22 15:11:37 +0100

    Add site-map

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,24 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
-
+import sitemap from "@astrojs/sitemap";
 const file = fileURLToPath(new URL('package.json', import.meta.url));
 const json = readFileSync(file, 'utf8');
 const pkg = JSON.parse(json);
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     define: {
-      "import.meta.env.PKG_VERSION": JSON.stringify(pkg.version),
-    },
+      "import.meta.env.PKG_VERSION": JSON.stringify(pkg.version)
+    }
   },
+  site: 'https://boxersteavee.dev',
   integrations: [partytown({
     // Adds dataLayer.push as a forwarding-event.
     config: {
       forward: ["dataLayer.push"]
     }
-  }), solidJs(), tailwind()]
+  }), solidJs(), tailwind(), sitemap()]
 });
